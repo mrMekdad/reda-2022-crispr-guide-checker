78f1e31e83a04a57936eb6ac6b871b244e17b6b3
mrMekdad <mekdadr6@gmail.com>
2022-02-14 02:07:00 +0100

    crispr-guide-checker: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2022-crispr-guide-checker",
+    version="0.1.0",
+    description="Reference management and rule-based checks for guide candidate review.",
+    author="Red@",
+    packages=["crispr_guide_checker"],
+    package_dir={"": "src"},
+)
